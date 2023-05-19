7d3c0fb52e3ac8f3eacefbbb7e8da47bf37c8f6c
AlphaDureku <templanzamark2002@gmail.com>
2023-05-19 21:44:01 +0800

    push

--- utils/collectionOfFunctions.js
+++ utils/collectionOfFunctions.js
@@ -1,7 +1,7 @@
 exports.upperCaseFirstLetter = (params) => {
   const words = params.split(" ");
   const capitalizedWords = words.map((word) => {
-    return word.charAt(0).toUpperCase() + word.slice(1);
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
   });
   return capitalizedWords.join(" ");
 };
